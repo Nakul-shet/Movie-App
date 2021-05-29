af7a1543ef4359673a7a5ed92b35c490610c6e65
nakul <nakulnshet1@gmail.com>
2021-05-29 20:40:28 +0530

    color funtionality

--- src/component/movie.jsx
+++ src/component/movie.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import "../App.css"
 
 function Movie(props){
+
+    function popularity(vote){
+        if(vote >= 7.5){
+            return "gold"
+        }else if(vote >= 5){
+            return "orange"
+        }else {
+            return "red"
+        }
+    }
+
     return(
         
             <div className="movie">
                 <img src={props.image} alt="movie_image"/>
                 <div className="movie-info">
                     <h1>{props.title}</h1>
-                    <span className="green">{props.vote_average}</span>
+                    <span className={popularity(props.vote_average)}>{props.vote_average}</span>
                 </div>
                 <div className="overview">
                     <h2>Overview</h2>
